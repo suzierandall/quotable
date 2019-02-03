51c25f0ea25edcce4e3be76738c8909ca0911191
suzierandall <suzierandall@users.noreply.github.com>
2019-02-03 17:21:42 +0000

    Add missing comments for class and methods

--- app/quote.php
+++ app/quote.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Class Quote, creates nonsense quotes
+ * @author Suzie Randall
+ * @version 0.1
+ */
+
 include_once('lib/dictionary.php');
 
 class Quote {
@@ -10,10 +16,18 @@ class Quote {
 		$this->populate_patterns();
 	}
 
-	public function get_title() {
+	/**
+	 * Get a short title for the current quote
+	 * @return string - title for the quote
+	 */
+	public function get_title(): string {
 		return sprintf('The %s', $this->m_title);
 	}
 
+	/**
+	 * Get a quote
+	 * @return string - the quote or null on failure
+	 */
 	public function get_quote(): ?string {
 		$rv = null;
 		$entries = $this->get_dictionary();
@@ -31,6 +45,11 @@ class Quote {
 		return $rv;
 	}
 
+	/**
+	 * Chooses one item from an indexed array
+	 * @param array options - the array from which the item will be chosen
+	 * @return mixed - the value of the chosen item
+	 */
 	private static function choose(array $options) {
 		if (empty($options)) {
 			return null;
@@ -40,16 +59,29 @@ class Quote {
 		return $options[$index];
 	}
 
+	/**
+	 * Populate a list with the names of all of the quote pattern methods
+	 * @return none
+	 */
 	private function populate_patterns() {
 		$methods = get_class_methods($this);
-		$this->m_patterns = static::purge($methods, 'static::is_patternable');
+		$this->m_patterns = static::purge($methods, 'static::is_patterned');
 	}
 
-	private static function is_patternable($name) {
+	/**
+	 * Is this method name one of the quote pattern methods?
+	 * @param string name - the method name to check
+	 * @return bool - true if it is a quote pattern method; false otherwise
+	 */
+	private static function is_patterned($name): bool {
 		return strpos($name, 'get_quote_pattern_') === 0;
 	}
 
-	private function get_dictionary() {
+	/**
+	 * Get the dictionary chapters for a randomly selected pattern
+	 * @return array - the dictionary chapters
+	 */
+	private function get_dictionary(): ?array {
 		$rv = null;
 		$pattern_callable = $this->choose($this->m_patterns);
 		if (is_callable([$this, $pattern_callable])) {
@@ -59,22 +91,46 @@ class Quote {
 		return $rv;
 	}
 
+	/**
+	 * Get the dictionary chapter pattern for a long quote
+	 * @return array - the array keys for the required chapters
+	 */
 	private function get_quote_pattern_long(): array {
 		return ['sub', 'fix', 'adj', 'comma', 'subord', 'sub', 'verb', 'poss', 'adj', 'noun'];
 	}
 
+	/**
+	 * Get the dictionary chapter pattern for a longish quote
+	 * @return array - the array keys for the required chapters
+	 */
 	private function get_quote_pattern_longish(): array {
 		return ['sub', 'fix', 'adj', 'coord', 'adj', 'comma', 'coord', 'sub', 'verb', 'poss', 'adj', 'noun'];
 	}
 
+	/**
+	 * Get the dictionary chapter pattern for a longer quote
+	 * @return array - the array keys for the required chapters
+	 */
 	private function get_quote_pattern_longer(): array {
 		return ['sub', 'fix', 'adj', 'coord', 'adj', 'comma', 'subord', 'sub', 'verb', 'poss', 'adj', 'noun'];
 	}
 
+	/**
+	 * Set the title for the current quote
+	 * @param array quote - the quote, itemised by chapter
+	 * @return void
+	 */
 	private function set_long_quote_title(array $quote): void {
+		// @todo FIXME: key word positions should be set per pattern
 		$this->m_title = $quote[8];
 	}
 
+	/**
+	 * Filters and re-indexes indexed arrays
+	 * @param array vals - array to be purged
+	 * @param string $callback - optional callback to pass to array_filter
+	 * @return string - the purged and reindexed array
+	 */
 	private static function purge(array $vals, string $callback = null) {
 		$vals = !empty($callback)
 			? array_filter($vals, $callback)
@@ -82,6 +138,11 @@ class Quote {
 		return array_values($vals);
 	}
 
+	/**
+	 * Get the required dictionary chapters for the requested quote pattern
+	 * @param array pattern - an array of required chapters
+	 * @return array - the dictionary chapters in the order specified by the pattern
+	 */
 	private function get_dictionary_by_pattern(array $pattern): array {
 		$vals = [];
 		foreach($pattern as $key) {
@@ -90,7 +151,12 @@ class Quote {
 		return static::purge($vals);
 	}
 
-	private function get_dictionary_chapter(string $key) {
+	/**
+	 * Get a specific dictionary chapter
+	 * @param string key - the array key for the required chapter
+	 * @return array - the dictionary chapter
+	 */
+	private function get_dictionary_chapter(string $key): ?array {
 		static $dictionary;
 		if (is_null($dictionary)) {
 			$dictionary = $this->get_base_dictionary();
@@ -98,6 +164,10 @@ class Quote {
 		return $dictionary[$key] ?? null;
 	}
 
+	/**
+	 * Get an associative array containing all of the dictionary chapters
+	 * @return array - the dictionary chapters
+	 */
 	private function get_base_dictionary(): array {
 		return [
 			'sub' => get_subject_pronouns(),
