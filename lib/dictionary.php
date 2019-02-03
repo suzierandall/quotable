f43c419093198bfe7969e6be22751bb43504c128
suzierandall <suzierandall@users.noreply.github.com>
2019-02-03 02:32:10 +0000

    Separate dictionary initialisation from dictionary access

--- lib/dictionary.php
+++ lib/dictionary.php
@@ -1,34 +1,51 @@
 <?php
 
-function get_dictionary(): array {
-	$subject = ['she', 'he', 'I', 'you'];
-	$make_it_fit_verb = ['looked', 'felt', 'seemed', 
-		'became', 'appeared'];
-	$adjective = ['giant', 'miniscule', 'bright', 'dull', 'hopeful', 
+function get_subject_pronouns(): array {
+	return ['she', 'he', 'I', 'you'];
+}
+
+function get_fixer(): array {
+	return ['looked', 'felt', 'seemed', 'became', 'appeared'];
+}
+
+function get_adjectives(): array {
+	return ['giant', 'miniscule', 'bright', 'dull', 'hopeful', 
 		'hungry', 'pretty', 'quiet', 'noisy', 'peaceful', 'boring', 
 		'insane', 'wacky', 'frightened', 'sweet', 'perfect', 'amused', 
 		'silly', 'changeable'];
-	$conjunction = ['as', 'whilst', 'before', 'after', 'when'];
-	$verb = ['opened', 'closed', 'borrowed', 'returned to', 'hoped for', 
+}
+
+function get_conjunctions(): array {
+	return ['as', 'whilst', 'before', 'after', 'when'];
+}
+
+function get_verbs(): array {
+	return ['opened', 'closed', 'borrowed', 'returned to', 'hoped for', 
 		'needed', 'placed', 'added', 'removed', 'watched', 'ignored', 
 		'laid down', 'picked up', 'wondered about', 'created', 'released', 
 		'found', 'considered', 'pondered'];
-	$possessive = ['the', 'their', 'our', 'his', 'her', 'a', 'my', 'your'];
-	$noun = ['chair', 'house', 'car', 'horse', 'donkey', 'swimming pool', 
+}
+
+function get_possessive_pronouns(): array {
+	return ['the', 'their', 'our', 'his', 'her', 'a', 'my', 'your'];
+}
+
+function get_nouns(): array {
+	return ['chair', 'house', 'car', 'horse', 'donkey', 'swimming pool', 
 		'tree', 'cliff', 'piano', 'guitar', 'fish', 'ocean', 'bowl', 'plate', 
 		'wand', 'hat', 'door', 'cinema', 'banana', 'treacle', 'pond'];
+}
 
-	$entries = [
-		$subject, 
-		$make_it_fit_verb, 
-		$adjective, 
-		$conjunction, 
-		$subject, 
-		$verb, 
-		$possessive, 
-		$adjective, 
-		$noun
+function get_dictionary(): array {
+	return [
+		get_subject_pronouns(), 
+		get_fixer(), 
+		get_adjectives(), 
+		get_conjunctions(), 
+		get_subject_pronouns(), 
+		get_verbs(), 
+		get_possessive_pronouns(), 
+		get_adjectives(), 
+		get_nouns()
 	];
-
-	return $entries;	
 }
